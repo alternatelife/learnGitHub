ef00a65ee168f0f94ebde7484fe1a28ad61655ca
mohammadehtesham <m.eht.24@gmail.com>
2016-05-09 20:32:11 -0400

    trying user input data for 4th record
    Work is incomplete, continue from import System.io.BufferedReader as
    well as method getUserInput().

--- src/com/mohammadehtesham/learnGitHub/Main.java
+++ src/com/mohammadehtesham/learnGitHub/Main.java
@@ -1,12 +1,19 @@
 package com.mohammadehtesham.learnGitHub;
 
+import System.io.BufferedReader;
+import System.io.InputSystem
+
 public class Main {
 
     public static void main(String[] args) {
-	    StudentsGrades [] pISES = new StudentsGrades[3];
+	    StudentsGrades [] pISES = new StudentsGrades[4];
         pISES[0] = create("Mohammad Ehtesham", 94,91,92);
         pISES[1] = create("Farah Sethi", 93, 94, 90);
         pISES[2] = create("Ahmed Ehtesham", 93, 92, 95);
+        pISES[3].name = DataInput("Please enter name here: ");
+        pISES[3].compSciMarks = DataInput("Please enter Computer Science marks here: ");
+        pISES[3].englishMarks = DataInput("Please enter English Marks here: ");
+        pISES[3].mathMarks = DataInput("Please enter Mathematics Marks here: ");
         String nameOfHighestScorer = "NA";
         int highestTotal = 0;
 
@@ -37,6 +44,14 @@ public class Main {
         newRecord.compSciMarks = compSciMarks;
         return newRecord;
 
+    }
+    public static StudentGrades getUserInput(){
+        StudentsGrades userInput = new StudentsGrades();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Please enter the name of the student: ");
+        userInput.name = reader.readLine();
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Please enter the Computer Science marks: ");
     }
 
 }
